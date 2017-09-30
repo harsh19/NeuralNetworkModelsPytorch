e7ab3b0574919c961e43d72b6280324f18e75fc6
Harsh Jhamtani <harshjhamtani@gmail.com>
2017-09-30 15:03:42 -0400

    Models accomodate mini-batching now

--- models.py
+++ models.py
@@ -29,14 +29,18 @@ class EncoderRNN(nn.Module):
 
     def forward(self, input, hidden):
         #print("forward: " + str(input.data.shape))
-        embedded = self.embedding(input).view(1, 1, -1)
+        embedded = self.embedding(input).view(1, 5, -1)
         output = embedded
+        print("forward: --hidden: " + str(hidden.data.size()))
+        print("forward: embedded: " + str(embedded.data.size()))
         for i in range(self.n_layers):
             output, hidden = self.gru(output, hidden)
+        print("forward: output: " + str(output.data.size()))
+        print("forward: hidden: " + str(hidden.data.size()))
         return output, hidden
 
-    def initHidden(self):
-        result = Variable(torch.zeros(1, 1, self.hidden_size))
+    def initHidden(self, batch_size):
+        result = Variable(torch.zeros(1, batch_size, self.hidden_size)) # seq length, batchsize, hidden size
         if use_cuda:
             return result.cuda()
         else:
@@ -91,30 +95,33 @@ class AttnDecoderRNN(nn.Module):
         self.out = nn.Linear(self.hidden_size, self.output_size)
 
     def computeAttention(self, previous_hidden, encoder_outputs):
-        query = self.attn(previous_hidden) # b,1,h
-        #print("query: " + str(query.data.size()))  
+        # encoder_outputs: m, b, h
+        query = self.attn(previous_hidden) # 1,b,h
+        print("query: " + str(query.data.size()))  
         #print("torch.transpose(query, 1, 2) : " + str(torch.transpose(query, 1, 2).data.size()))  
-        attn_weights = torch.bmm( encoder_outputs.unsqueeze(0), torch.transpose(query, 1, 2) ) # b,m,h x b,h,1 => b,m,1
-        #print("attn_weights: " + str(attn_weights.data.size()))  
+        attn_weights = torch.bmm( torch.transpose(encoder_outputs,0,1), torch.transpose(torch.transpose(query, 0, 1), 1,2) ) # b,m,h x b,h,1 => b,m,1
+        print("attn_weights: " + str(attn_weights.data.size()))  
         attn_weights = attn_weights.squeeze(2) # b,m,1 => b,m
         attn_weights = F.softmax(attn_weights) # softmax
-        #print("attn_weights after softmax: " + str(attn_weights.data.size()))  
-        context = torch.bmm(attn_weights.unsqueeze(0),
-                                 encoder_outputs.unsqueeze(0)) 
-        #print("context: " + str(context.data.size()))     
+        print("attn_weights after softmax: " + str(attn_weights.data.size()))  
+        context = torch.bmm(attn_weights.unsqueeze(1),
+                                 torch.transpose(encoder_outputs,0,1) ) # b,m->b,1,m  m,b,h -> b,m,h  : b,1,m x b,m,h => b,1,h
+        context = context.squeeze() # b,h
+        print("context: " + str(context.data.size()))     
         return context, attn_weights
 
 
     def forward(self, input, hidden, encoder_output, encoder_outputs):
-        embedded = self.embedding(input).view(1, 1, -1)
+        embedded = self.embedding(input) #.view(1, 5, -1)
         embedded = self.dropout(embedded)
         #print("========================")
 
-        '''print("input: " + str(input.data.shape))
-        print("forward emb: " + str(embedded.data.shape))
-        print("forward: " + str(self.embedding(input).data.size()))
-        print("encoder_outputs: " + str(encoder_outputs.data.size()))
-        '''
+        print("input: " + str(input.data.shape)) #1,b
+        print("forward emb: " + str(embedded.data.shape)) # 1,b,es
+        #print("forward: " + str(self.embedding(input).data.size()))
+        print("encoder_outputs: " + str(encoder_outputs.data.size())) # 10,b,h_enc
+        print("hidden: " + str(hidden.data.size())) # 1,b,h_dec
+        
 
         '''attn_weights = F.softmax(
             self.attn(torch.cat((embedded[0], hidden[0]), 1)))
@@ -125,14 +132,19 @@ class AttnDecoderRNN(nn.Module):
         '''       
         context, attn_weights = self.computeAttention(hidden, encoder_outputs)
 
-        output = torch.cat((embedded[0], context[0]), 1)
-        output = self.attn_combine(output).unsqueeze(0)
+        output = torch.cat((embedded[0], context), 1) # b,2*h
+        output = self.attn_combine(output) # b,2h x 2h,h => b,h
+        output = output.unsqueeze(0) # 1,b,h
+        print("output after context embedding mix: " + str(output.data.size())) # 1,b,h_dec
+
 
         for i in range(self.n_layers):
             output = F.relu(output)
             output, hidden = self.gru(output, hidden)
 
         output = F.log_softmax(self.out(output[0]))
+        print("output after softmax: " + str(output.data.size())) # 1,b,h_dec
+
         return output, hidden, attn_weights
 
     def initHidden(self):
--- solver.py
+++ solver.py
@@ -16,7 +16,7 @@ import torch.nn as nn
 from torch.autograd import Variable
 from torch import optim
 import torch.nn.functional as F
-
+import pickle
 
 import prepro
 from models import EncoderRNN
@@ -34,30 +34,39 @@ class Solver:
 
 
     def train(self, input_variable, target_variable, encoder, decoder, encoder_optimizer, decoder_optimizer, criterion, max_length):
-        encoder_hidden = encoder.initHidden()
 
         encoder_optimizer.zero_grad()
         decoder_optimizer.zero_grad()
 
-        input_length = input_variable.size()[0]
-        target_length = target_variable.size()[0]
+        batch_size = input_variable.size()[0]
+        input_length = input_variable.size()[1]  # input -> b,m,1
+        target_length = target_variable.size()[1]
+        #print("input_variable.size()="+str(input_variable.size()))
 
-        encoder_outputs = Variable(torch.zeros(max_length, encoder.hidden_size))
+        encoder_hidden = encoder.initHidden(batch_size) # 1,b,h
+        print("encoder_hidden.size()="+str(encoder_hidden.size()))
+        encoder_outputs = Variable(torch.zeros(max_length, batch_size, encoder.hidden_size))
         encoder_outputs = encoder_outputs.cuda() if self.use_cuda else encoder_outputs
 
         loss = 0
 
         for ei in range(input_length):
+            inp = input_variable[:,ei,:] # b,1
+            print("inp.size()="+str(inp.size()))
             encoder_output, encoder_hidden = encoder(
-                input_variable[ei], encoder_hidden)
-            encoder_outputs[ei] = encoder_output[0][0]
+                inp, encoder_hidden)
+            print("encoder_output.size()="+str(encoder_output.size()))
+            encoder_outputs[ei] = encoder_output[0]
+        print("encoder_outputs.size()="+str(encoder_outputs.size()))
+        print("="*66)
 
-        decoder_input = Variable(torch.LongTensor([[self.SOS_token]]))
+        decoder_input = Variable(torch.LongTensor([[self.SOS_token]*5]))
         decoder_input = decoder_input.cuda() if self.use_cuda else decoder_input
 
-        decoder_hidden = encoder_hidden
+        decoder_hidden = encoder_hidden # can also use last encoder output
 
         use_teacher_forcing = True if random.random() < self.teacher_forcing_ratio else False
+        use_teacher_forcing = True
 
         if use_teacher_forcing:
             # Teacher forcing: Feed the target as the next input
@@ -111,15 +120,17 @@ class Solver:
 
         encoder_optimizer = optim.SGD(encoder.parameters(), lr=learning_rate)
         decoder_optimizer = optim.SGD(decoder.parameters(), lr=learning_rate)
-        training_pairs = [ self.data_preparer.variablesFromPair(random.choice(self.data_preparer.pairs))
-                          for i in range(n_iters)]
+        #training_pairs = [ self.data_preparer.variablesFromPair(random.choice(self.data_preparer.pairs))
+        #                  for i in range(n_iters)]
+        training_pairs = [ self.data_preparer.variablesFromPairs( [random.choice(self.data_preparer.pairs)]*5 )
+                          for i in range(n_iters)]                          
         criterion = nn.NLLLoss()
 
         for iter in range(1, n_iters + 1):
             training_pair = training_pairs[iter - 1]
             input_variable = training_pair[0]
-            print("input_variable = "+str(input_variable))
             target_variable = training_pair[1]
+            print("input_variable = "+str(input_variable))
 
             loss = self.train(input_variable, target_variable, encoder,
                          decoder, encoder_optimizer, decoder_optimizer, criterion, self.MAX_LENGTH)
@@ -248,98 +259,105 @@ class Solver:
         print('output =', ' '.join(output_words))
         showAttention(input_sentence, output_words, attentions)
 
-    def main(self):
+    def main(self, mode):
 
         ## prepro
-        self.data_preparer = data_preparer = prepro.Prepro()
-        input_lang, output_lang, pairs = data_preparer.getData()
-        print("="*50)
-        self.MAX_LENGTH = data_preparer.MAX_LENGTH
-        print("="*50)
+        if mode=="prepro":
+            self.data_preparer = data_preparer = prepro.Prepro()
+            data_preparer.getData()
+            pickle.dump(data_preparer, open(u'data_preparer.p','wb'))
+        else:
+            data_preparer = pickle.load( open(u'data_preparer.p','rb') )
+            self.data_preparer = data_preparer
+            input_lang, output_lang, pairs = data_preparer.input_lang, data_preparer.output_lang, \
+                data_preparer.pairs
+            print("="*50)
+            self.MAX_LENGTH = data_preparer.MAX_LENGTH
+            print("="*50)
 
-        ######################################################################
-        # Training and Evaluating
-        # =======================
-        #
-        # With all these helper functions in place (it looks like extra work, but
-        # it's easier to run multiple experiments easier) we can actually
-        # initialize a network and start training.
-        #
-        # Remember that the input sentences were heavily filtered. For this small
-        # dataset we can use relatively small networks of 256 hidden nodes and a
-        # single GRU layer. After about 40 minutes on a MacBook CPU we'll get some
-        # reasonable results.
-        #
-        # .. Note:: 
-        #    If you run this notebook you can train, interrupt the kernel,
-        #    evaluate, and continue training later. Comment out the lines where the
-        #    encoder and decoder are initialized and run ``trainIters`` again.
-        #
+            ######################################################################
+            # Training and Evaluating
+            # =======================
+            #
+            # With all these helper functions in place (it looks like extra work, but
+            # it's easier to run multiple experiments easier) we can actually
+            # initialize a network and start training.
+            #
+            # Remember that the input sentences were heavily filtered. For this small
+            # dataset we can use relatively small networks of 256 hidden nodes and a
+            # single GRU layer. After about 40 minutes on a MacBook CPU we'll get some
+            # reasonable results.
+            #
+            # .. Note:: 
+            #    If you run this notebook you can train, interrupt the kernel,
+            #    evaluate, and continue training later. Comment out the lines where the
+            #    encoder and decoder are initialized and run ``trainIters`` again.
+            #
 
-        hidden_size = 256
-        encoder1 = EncoderRNN(input_lang.n_words, hidden_size)
-        attn_decoder1 = AttnDecoderRNN(hidden_size, output_lang.n_words,
-                                       1, dropout_p=0.1, max_length=self.MAX_LENGTH)
+            hidden_size = 256
+            encoder1 = EncoderRNN(input_lang.n_words, hidden_size)
+            attn_decoder1 = AttnDecoderRNN(hidden_size, output_lang.n_words,
+                                           1, dropout_p=0.1, max_length=self.MAX_LENGTH)
 
-        if self.use_cuda:
-            encoder1 = encoder1.cuda()
-            attn_decoder1 = attn_decoder1.cuda()
+            if self.use_cuda:
+                encoder1 = encoder1.cuda()
+                attn_decoder1 = attn_decoder1.cuda()
 
-        self.trainIters(encoder1, attn_decoder1, 75000, print_every=5000)
+            self.trainIters(encoder1, attn_decoder1, 75000, print_every=5000)
 
-        ######################################################################
-        #
+            ######################################################################
+            #
 
-        self.evaluateRandomly(encoder1, attn_decoder1)
-
-
-        ######################################################################
-        # Visualizing Attention
-        # ---------------------
-        #
-        # A useful property of the attention mechanism is its highly interpretable
-        # outputs. Because it is used to weight specific encoder outputs of the
-        # input sequence, we can imagine looking where the network is focused most
-        # at each time step.
-        #
-        # You could simply run ``plt.matshow(attentions)`` to see attention output
-        # displayed as a matrix, with the columns being input steps and rows being
-        # output steps:
-        #
-
-        output_words, attentions = self.evaluate(
-            encoder1, attn_decoder1, "je suis trop froid .")
-        plt.matshow(attentions.numpy())
-
-        self.evaluateAndShowAttention("elle a cinq ans de moins que moi .")
-
-        self.evaluateAndShowAttention("elle est trop petit .")
-
-        self.evaluateAndShowAttention("je ne crains pas de mourir .")
-
-        self.evaluateAndShowAttention("c est un jeune directeur plein de talent .")
+            self.evaluateRandomly(encoder1, attn_decoder1)
 
 
-        ######################################################################
-        # Exercises
-        # =========
-        #
-        # -  Try with a different dataset
-        #
-        #    -  Another language pair
-        #    -  Human → Machine (e.g. IOT commands)
-        #    -  Chat → Response
-        #    -  Question → Answer
-        #
-        # -  Replace the embeddings with pre-trained word embeddings such as word2vec or
-        #    GloVe
-        # -  Try with more layers, more hidden units, and more sentences. Compare
-        #    the training time and results.
-        # -  If you use a translation file where pairs have two of the same phrase
-        #    (``I am test \t I am test``), you can use this as an autoencoder. Try
-        #    this:
-        #
-        #    -  Train as an autoencoder
-        #    -  Save only the Encoder network
-        #    -  Train a new Decoder for translation from there
-        #
+            ######################################################################
+            # Visualizing Attention
+            # ---------------------
+            #
+            # A useful property of the attention mechanism is its highly interpretable
+            # outputs. Because it is used to weight specific encoder outputs of the
+            # input sequence, we can imagine looking where the network is focused most
+            # at each time step.
+            #
+            # You could simply run ``plt.matshow(attentions)`` to see attention output
+            # displayed as a matrix, with the columns being input steps and rows being
+            # output steps:
+            #
+
+            output_words, attentions = self.evaluate(
+                encoder1, attn_decoder1, "je suis trop froid .")
+            plt.matshow(attentions.numpy())
+
+            self.evaluateAndShowAttention("elle a cinq ans de moins que moi .")
+
+            self.evaluateAndShowAttention("elle est trop petit .")
+
+            self.evaluateAndShowAttention("je ne crains pas de mourir .")
+
+            self.evaluateAndShowAttention("c est un jeune directeur plein de talent .")
+
+
+            ######################################################################
+            # Exercises
+            # =========
+            #
+            # -  Try with a different dataset
+            #
+            #    -  Another language pair
+            #    -  Human → Machine (e.g. IOT commands)
+            #    -  Chat → Response
+            #    -  Question → Answer
+            #
+            # -  Replace the embeddings with pre-trained word embeddings such as word2vec or
+            #    GloVe
+            # -  Try with more layers, more hidden units, and more sentences. Compare
+            #    the training time and results.
+            # -  If you use a translation file where pairs have two of the same phrase
+            #    (``I am test \t I am test``), you can use this as an autoencoder. Try
+            #    this:
+            #
+            #    -  Train as an autoencoder
+            #    -  Save only the Encoder network
+            #    -  Train a new Decoder for translation from there
+            #
