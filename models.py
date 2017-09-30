edfb406e0448ea9ae85b04451accf96263b5e99f
Harsh Jhamtani <harshjhamtani@gmail.com>
2017-09-30 13:11:02 -0400

    changed attention formulation

--- models.py
+++ models.py
@@ -83,32 +83,49 @@ class AttnDecoderRNN(nn.Module):
         self.max_length = max_length
 
         self.embedding = nn.Embedding(self.output_size, self.hidden_size)
-        self.attn = nn.Linear(self.hidden_size * 2, self.max_length)
+        self.attn = nn.Linear(self.hidden_size, self.hidden_size) 
+        #nn.Linear(self.hidden_size * 2, self.max_length)
         self.attn_combine = nn.Linear(self.hidden_size * 2, self.hidden_size)
         self.dropout = nn.Dropout(self.dropout_p)
         self.gru = nn.GRU(self.hidden_size, self.hidden_size)
         self.out = nn.Linear(self.hidden_size, self.output_size)
 
+    def computeAttention(self, previous_hidden, encoder_outputs):
+        query = self.attn(previous_hidden) # b,1,h
+        #print("query: " + str(query.data.size()))  
+        #print("torch.transpose(query, 1, 2) : " + str(torch.transpose(query, 1, 2).data.size()))  
+        attn_weights = torch.bmm( encoder_outputs.unsqueeze(0), torch.transpose(query, 1, 2) ) # b,m,h x b,h,1 => b,m,1
+        #print("attn_weights: " + str(attn_weights.data.size()))  
+        attn_weights = attn_weights.squeeze(2) # b,m,1 => b,m
+        attn_weights = F.softmax(attn_weights) # softmax
+        #print("attn_weights after softmax: " + str(attn_weights.data.size()))  
+        context = torch.bmm(attn_weights.unsqueeze(0),
+                                 encoder_outputs.unsqueeze(0)) 
+        #print("context: " + str(context.data.size()))     
+        return context, attn_weights
+
+
     def forward(self, input, hidden, encoder_output, encoder_outputs):
         embedded = self.embedding(input).view(1, 1, -1)
         embedded = self.dropout(embedded)
-        print("========================")
+        #print("========================")
 
-        print("input: " + str(input.data.shape))
+        '''print("input: " + str(input.data.shape))
         print("forward emb: " + str(embedded.data.shape))
         print("forward: " + str(self.embedding(input).data.size()))
         print("encoder_outputs: " + str(encoder_outputs.data.size()))
-      
+        '''
 
-        attn_weights = F.softmax(
+        '''attn_weights = F.softmax(
             self.attn(torch.cat((embedded[0], hidden[0]), 1)))
         print("attn_weights: " + str(attn_weights.data.size()))        
         attn_applied = torch.bmm(attn_weights.unsqueeze(0),
                                  encoder_outputs.unsqueeze(0))
-        print("attn_applied: " + str(attn_applied.data.size()))        
+        print("attn_applied: " + str(attn_applied.data.size())) 
+        '''       
+        context, attn_weights = self.computeAttention(hidden, encoder_outputs)
 
-
-        output = torch.cat((embedded[0], attn_applied[0]), 1)
+        output = torch.cat((embedded[0], context[0]), 1)
         output = self.attn_combine(output).unsqueeze(0)
 
         for i in range(self.n_layers):
